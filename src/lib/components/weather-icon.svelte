e3fa24536ec764846eb634ba7d05fdfc818c8a1e
dinghino <dinghino@users.noreply.github.com>
2025-05-28 14:54:20 +0200

    ✨ weather icon tooltip
    - add shadcn tooltip
    - cleanup icon
    - add weather text to icon

--- src/lib/components/weather-icon.svelte
+++ src/lib/components/weather-icon.svelte
@@ -10,6 +10,8 @@
     CloudLightning,
     Droplets
   } from '@lucide/svelte'
+  import * as Tooltip from '$components/ui/tooltip'
+  import api from '$lib/api'
 
   type WeatherIconProps = {
     code: number
@@ -20,35 +22,41 @@
   const { code, size = 'lg', className }: WeatherIconProps = $props()
 
   const clsName = $derived(
-    cn(
-      {
-        'size-4': size === 'xs',
-        'size-6': size === 'sm',
-        'size-10': size === 'md',
-        'size-16': size === 'lg',
-        'size-24': size === 'xl'
-      },
-      className
-    )
+    cn('hover:stroke-primary transition-colors duration-200', className, {
+      'size-4': size === 'xs',
+      'size-6': size === 'sm',
+      'size-10': size === 'md',
+      'size-16': size === 'lg',
+      'size-24': size === 'xl'
+    })
   )
 </script>
 
 <div class={cn('flex-shrink-0', {})}>
-  {#if code === 0 || code === 1}
-    <Sun class={clsName} />
-  {:else if code >= 2 && code <= 3}
-    <CloudSun class={clsName} />
-  {:else if code >= 45 && code <= 49}
-    <CloudFog class={clsName} />
-  {:else if code >= 50 && code <= 59}
-    <Droplets class={clsName} />
-  {:else if code >= 60 && code <= 69}
-    <CloudRain class={clsName} />
-  {:else if code >= 70 && code <= 79}
-    <CloudSnow class={clsName} />
-  {:else if code >= 95 && code <= 99}
-    <CloudLightning class={clsName} />
-  {:else}
-    <Cloud class={clsName} />
-  {/if}
+  <Tooltip.Provider>
+    <Tooltip.Root>
+      <Tooltip.Trigger>
+        {#if code === 0 || code === 1}
+          <Sun class={clsName} />
+        {:else if code >= 2 && code <= 3}
+          <CloudSun class={clsName} />
+        {:else if code >= 45 && code <= 49}
+          <CloudFog class={clsName} />
+        {:else if code >= 50 && code <= 59}
+          <Droplets class={clsName} />
+        {:else if code >= 60 && code <= 69}
+          <CloudRain class={clsName} />
+        {:else if code >= 70 && code <= 79}
+          <CloudSnow class={clsName} />
+        {:else if code >= 95 && code <= 99}
+          <CloudLightning class={clsName} />
+        {:else}
+          <Cloud class={clsName} />
+        {/if}
+      </Tooltip.Trigger>
+      <Tooltip.Content>
+        {api.weather.getWeatherDescription(code)}
+      </Tooltip.Content>
+    </Tooltip.Root>
+  </Tooltip.Provider>
 </div>
