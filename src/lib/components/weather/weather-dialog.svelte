eb1c8709fb04a572d075c5eed25ec11731f3b827
dinghino <dinghino@users.noreply.github.com>
2025-05-29 22:15:50 +0200

    🐛 fix dialog on small screens
    dialog wasn't scrolling. no scrolling makes me sad.

--- src/lib/components/weather/weather-dialog.svelte
+++ src/lib/components/weather/weather-dialog.svelte
@@ -18,19 +18,18 @@
   }
 
   let { location, data, class: className, trigger, current }: Props = $props()
-
 </script>
 
 <Dialog.Root>
   <Dialog.Trigger class="cursor-pointer">
-      {@render trigger()}
+    {@render trigger()}
   </Dialog.Trigger>
-  <Dialog.Content class={cn(className)}>
-    <Tabs.Root value="current">
-      <Dialog.Header>
-        <Dialog.Title>{location.name}</Dialog.Title>
-      </Dialog.Header>
-      <Tabs.List class="bg-accent dark:bg-accent/50 my-4 w-full flex-1 px-4">
+  <Dialog.Content class={cn(className, ' max-h-[80vh] gap-0 overflow-y-auto p-0 pb-4')}>
+    <Dialog.Header class="bg-muted dark:bg-muted/50 inline-flex items-start px-4 py-6">
+      <Dialog.Title>{location.name}</Dialog.Title>
+    </Dialog.Header>
+    <Tabs.Root value="current" class="">
+      <Tabs.List class="bg-muted dark:bg-muted/50 mb-8 w-full flex-1 rounded-none">
         <Tabs.Trigger value="current" class="flex items-center gap-2 text-xs">Current</Tabs.Trigger>
         <Tabs.Trigger disabled value="hourly" class="flex items-center gap-2 text-xs">
           Hourly
@@ -38,11 +37,11 @@
         <Tabs.Trigger value="daily" class="flex items-center gap-2 text-xs">Daily</Tabs.Trigger>
       </Tabs.List>
 
-      <Tabs.Content value="current" class="flex-1">
+      <Tabs.Content value="current" class="flex-1 px-4">
         {@render current(data)}
       </Tabs.Content>
 
-      <Tabs.Content value="daily" class="flex-1">
+      <Tabs.Content value="daily" class="flex-1 px-4">
         <DailyForecast daily={data.daily} />
       </Tabs.Content>
     </Tabs.Root>
